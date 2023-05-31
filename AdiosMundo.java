f7768217b0ad0ee3a9c1b096b3d5daaad5ba390d
dbodonf01 <131864556+dbodonf01@users.noreply.github.com>
2023-05-31 09:33:05 +0200

    Update AdiosMundo.java

--- AdiosMundo.java
+++ AdiosMundo.java
@@ -3,6 +3,7 @@ public class AdiosMundo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Adios Juan");
+		System.out.println("Hola Jose");
 		
 	}
 
