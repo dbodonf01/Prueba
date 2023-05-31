0bf3dd3a00c497b8c6ec588a1e725ff5362ec190
dbodonf01 <131864556+dbodonf01@users.noreply.github.com>
2023-05-31 09:28:44 +0200

    Update holamundo.java

--- Prueba/src/Prueba1/holamundo.java
+++ Prueba/src/Prueba1/holamundo.java
@@ -5,6 +5,7 @@ public class holamundo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Mundo");
+		System.out.println("Hola Pedro");
 	}
 
 }
